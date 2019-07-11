2d347154d0728ac9d65baa611c72dd2faec91c41
Manasbi Parajuli <parajulimanasbi@gmail.com>
2019-07-10 23:05:46 -0400

    Added toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'resource-manager-client';
+  title = 'Resource Management';
 }
